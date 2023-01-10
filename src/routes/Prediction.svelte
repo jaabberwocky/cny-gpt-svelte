74c7c69dfe92bf277e973a781deeb93dfe74d892
tobias <tobias.leong@zendesk.com>
2023-01-10 08:21:04 +0800

    Change button styling

--- src/routes/Prediction.svelte
+++ src/routes/Prediction.svelte
@@ -35,7 +35,7 @@
 
 <div class="lucky-predictions">
 	<br />
-	<button on:click={handleClick}>Get Prediction</button>
+	<button class="button-3" on:click={handleClick}>💰 Get Prediction 💰</button>
 	<LuckyNumber {num} />
 	<LuckyPhrase {phrase} />
 	<LuckyHoroscope {horoscope} />
@@ -49,8 +49,61 @@
 	}
 
 	button {
-		max-width: 150px;
+		max-width: 250px;
 		/* centers the button */
 		margin: 0 auto;
+	}
+
+	/* CSS */
+	.button-3 {
+		appearance: none;
+		background-color: #2ea44f;
+		border: 1px solid rgba(27, 31, 35, 0.15);
+		border-radius: 6px;
+		box-shadow: rgba(27, 31, 35, 0.1) 0 1px 0;
+		box-sizing: border-box;
+		color: #fff;
+		cursor: pointer;
+		display: inline-block;
+		font-family: -apple-system, system-ui, 'Segoe UI', Helvetica, Arial, sans-serif,
+			'Apple Color Emoji', 'Segoe UI Emoji';
+		font-size: 14px;
+		font-weight: 600;
+		line-height: 20px;
+		padding: 6px 16px;
+		position: relative;
+		text-align: center;
+		text-decoration: none;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+		vertical-align: middle;
+		white-space: nowrap;
+	}
+
+	.button-3:focus:not(:focus-visible):not(.focus-visible) {
+		box-shadow: none;
+		outline: none;
+	}
+
+	.button-3:hover {
+		background-color: #2c974b;
+	}
+
+	.button-3:focus {
+		box-shadow: rgba(46, 164, 79, 0.4) 0 0 0 3px;
+		outline: none;
+	}
+
+	.button-3:disabled {
+		background-color: #94d3a2;
+		border-color: rgba(27, 31, 35, 0.1);
+		color: rgba(255, 255, 255, 0.8);
+		cursor: default;
+	}
+
+	.button-3:active {
+		background-color: #298e46;
+		box-shadow: rgba(20, 70, 32, 0.2) 0 1px 0 inset;
 	}
 </style>
