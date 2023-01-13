3d3c4570b69ced7dd79353cd8c0517751b196d6e
tobias <tobias@tobias-leong.dev>
2023-01-13 09:53:11 +0800

    Remove extraneous type import

--- src/routes/api/lucky-number/+server.ts
+++ src/routes/api/lucky-number/+server.ts
@@ -1,7 +1,6 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
 
-/** @type {import('./$types').RequestHandler} */
 export function GET({ url }: RequestEvent) {
 	const obj = {
 		number: Math.floor(Math.random() * (9999 - 0 + 1) + 0)
