9938af5d04f94536dddf27c36d3b3603eef8776d
tobias <tobias@tobias-leong.dev>
2023-01-14 16:32:48 +0800

    Add numAppearances store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,5 +3,6 @@ import { writable } from 'svelte/store';
 export const num = writable('');
 export const phrase = writable('');
 export const horoscope = writable('');
-export const previousResults = writable({});
+export const previousResults = writable([]);
 export const dataLoaded = writable(false);
+export const numAppearances = writable('');
