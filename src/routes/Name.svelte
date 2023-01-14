aea64a53e89aeb7d81c3623767e41fad3f83eb4d
tobias <tobias@tobias-leong.dev>
2023-01-14 17:52:42 +0800

    Add focus on init

--- src/routes/Name.svelte
+++ src/routes/Name.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	export let name: string;
+
+	function init(el: any) {
+		el.focus();
+	}
 </script>
 
 <div class="form-group">
 	<br />
 	<label for="name">Input name:</label>
-	<input bind:value={name} />
+	<input bind:value={name} use:init />
 </div>
 
 <style>
