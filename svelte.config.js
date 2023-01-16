02c4490a38509cc59d219e741ded6dfd9750cc7d
tobias <tobias@tobias-leong.dev>
2023-01-16 23:38:23 +0800

    Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,6 +10,6 @@ const config = {
 	kit: {
 		adapter: adapter()
 	}
-};
+}
 
 export default config;
